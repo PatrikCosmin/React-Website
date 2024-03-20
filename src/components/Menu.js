ff15d7840fdd4c724aab0be11334bef8d577aff2
Patrik Cosmin Vass <patrik.vass@capex.com>
2024-03-20 16:59:58 +0200

    Sprint - Menu Item Admin

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useUser } from '../context/UserContext';
-import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import '../styles/Menu.css';
+import '../styles/FormBtn.css';
+
 
 const baseURL = 'http://localhost:5000';
 
 const Menu = () => {
   const [menuItems, setMenuItems] = useState([]);
-  const [showModal, setShowModal] = useState(false);
   const [currentItem, setCurrentItem] = useState(null);
   const [formData, setFormData] = useState({ name: '', description: '', price: '', image: '' });
   const { user } = useUser();
@@ -18,10 +18,6 @@ const Menu = () => {
     try {
       const response = await axios.get(`${baseURL}/api/menu`);
       setMenuItems(response.data);
-      if (user && user.isAdmin !== 1) {
-        console.error('Error: userData does not exist or user is not admin');
-        // Handle error condition here
-      }
     } catch (error) {
       console.error('Error fetching menu data:', error);
     }
@@ -29,22 +25,20 @@ const Menu = () => {
 
   useEffect(() => {
     fetchMenuItems();
-  }, [showModal]);
+  }, []);
 
   const handleFormChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const openAddForm = () => {
-    setCurrentItem(null);
-    setFormData({ name: '', description: '', price: '', image: '' });
-    setShowModal(true);
-  };
-
   const openEditForm = (item) => {
     setCurrentItem(item);
     setFormData(item);
-    setShowModal(true);
+  };
+
+  const clearForm = () => {
+    setFormData({ name: '', description: '', price: '', image: '' });
+    setCurrentItem(null);
   };
 
   const handleSubmit = async (e) => {
@@ -55,7 +49,7 @@ const Menu = () => {
       } else {
         await axios.post(`${baseURL}/api/menu/add`, formData);
       }
-      setShowModal(false);
+      clearForm();
       fetchMenuItems(); // Fetch menu items again to reflect the changes
     } catch (error) {
       console.error('Error submitting menu item:', error);
@@ -73,9 +67,6 @@ const Menu = () => {
 
   return (
     <div className="menu-container">
-      {user && user.isAdmin === 1 && (
-        <Button onClick={openAddForm}>Add Product</Button>
-      )}
       {menuItems.map(item => (
         <div className="menu-item glowing-border" key={item.id}>
           <img src={item.image} alt={item.name} className="menu-image" />
@@ -85,28 +76,27 @@ const Menu = () => {
             <p>${item.price}</p>
             {user && user.isAdmin === 1 && (
               <>
-                <Button onClick={() => openEditForm(item)}>Edit</Button>
-                <Button onClick={() => handleDelete(item.id)}>Delete</Button>
+                <Button className='login-btn' onClick={() => openEditForm(item)}>Edit</Button>
+                <Button className='login-btn' onClick={() => handleDelete(item.id)}>Delete</Button>
               </>
             )}
           </div>
         </div>
       ))}
 
-      <Modal show={showModal} onHide={() => setShowModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>{currentItem ? 'Edit Menu Item' : 'Add Menu Item'}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
+      {user && user.isAdmin === 1 && (
+        <div className="admin-form-container menu-item">
+          <h2>{currentItem ? 'Edit Menu Item' : 'Add Menu Item'}</h2>
           <form onSubmit={handleSubmit}>
             <input type="text" name="name" value={formData.name} onChange={handleFormChange} placeholder="Name" />
             <input type="text" name="description" value={formData.description} onChange={handleFormChange} placeholder="Description" />
             <input type="number" name="price" value={formData.price} onChange={handleFormChange} placeholder="Price" />
             <input type="text" name="image" value={formData.image} onChange={handleFormChange} placeholder="Image URL" />
-            <Button type="submit">{currentItem ? 'Update' : 'Add'}</Button>
+            <Button className='login-btn' type="submit">{currentItem ? 'Update' : 'Add'}</Button>
+            <Button className='login-btn' onClick={clearForm}>Cancel</Button>
           </form>
-        </Modal.Body>
-      </Modal>
+        </div>
+      )}
     </div>
   );
 };
